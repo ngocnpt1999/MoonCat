05e000a80a178e49a6a7aa8c63e17261fff8bf9d
ngocnpt1999 <ngocnpt1999@gmail.com>
2019-05-08 15:43:37 +0700

    update 08/05/2019 03:43pm

--- MoonCat/MoonCat.Android/MainActivity.cs
+++ MoonCat/MoonCat.Android/MainActivity.cs
@@ -19,6 +19,8 @@ namespace MoonCat.Droid
 
             base.OnCreate(savedInstanceState);
 
+            Plugin.InputKit.Platforms.Droid.Config.Init(this, savedInstanceState);
+
             Xamarin.Essentials.Platform.Init(this, savedInstanceState);
             global::Xamarin.Forms.Forms.Init(this, savedInstanceState);
             LoadApplication(new App());
